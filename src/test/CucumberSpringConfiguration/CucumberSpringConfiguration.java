d92f13f15a2199f64df5eb7c59f58cdf406ab7de
MohammedHamedSheref <mohammedhamed@qawafel.sa>
2025-02-24 10:48:31 +0200

    initial commit

---
+++ src/test/CucumberSpringConfiguration/CucumberSpringConfiguration.java
@@ -0,0 +1,9 @@
+package CucumberSpringConfiguration;
+
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@CucumberContextConfiguration
+@SpringBootTest
+public class CucumberSpringConfiguration {
+}
